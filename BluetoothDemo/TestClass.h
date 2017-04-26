da223c0ca4eb578766c6f6f86edf861519ec78a4
全聚星 <18668460187@163.com>
2017-04-26 16:29:49 +0800

    提交最新的测试代码

---
+++ BluetoothDemo/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  BluetoothDemo
+//
+//  Created by lalala on 2017/4/26.
+//  Copyright © 2017年 lsh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ BluetoothDemo/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  BluetoothDemo
+//
+//  Created by lalala on 2017/4/26.
+//  Copyright © 2017年 lsh. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
